c40a7d37a39a95e6d471d820a068e8d0a4721108
prabhatgokloud <prabhat.uprety@gokloud.io>
2025-02-13 01:22:33 +0530

    Added-Cascade Deletion

---
+++ HouseBrokerApp.Infrastructure/Migrations/20250212195000_Added_Cascade_delete.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HouseBrokerApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Cascade_delete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PropertyFeatures_Properties_PropertyId",
+                table: "PropertyFeatures");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PropertyImages_Properties_PropertyId",
+                table: "PropertyImages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PropertyFeatures_Properties_PropertyId",
+                table: "PropertyFeatures",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PropertyImages_Properties_PropertyId",
+                table: "PropertyImages",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PropertyFeatures_Properties_PropertyId",
+                table: "PropertyFeatures");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PropertyImages_Properties_PropertyId",
+                table: "PropertyImages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PropertyFeatures_Properties_PropertyId",
+                table: "PropertyFeatures",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PropertyImages_Properties_PropertyId",
+                table: "PropertyImages",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id");
+        }
+    }
+}
